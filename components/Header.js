ce27a761a47a9cd554efad00e99a8be8b3a3d345
Jonas Knutsen <jonas@j10n.no>
2018-08-21 18:32:34 +0200

    Layout.js

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Header extends React.Component {
+  render () {
+    return (
+      <header>
+        This is a header
+      </header>
+    )
+  }
+}
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,32 @@
 import React from 'react'
+import Link from 'next/link'
 import fetch from 'isomorphic-fetch'
+
+import Layout from '../components/Layout'
 
 class Index extends React.Component{
   render () {
     const {posts} = this.props
     console.log(posts)
     return (
-      <div>
-        Hello
+      <Layout>
         {posts.map((p, i) => {
           return (
-            <div>{p.title.rendered}</div>
+            <article>
+              <h2><Link href={`/p/${p.link}`}><a>{p.title.rendered}</a></Link></h2>
+              <div className='meta'>
+                <span>Published {p.date}</span> <span>(Modified {p.modified})</span>
+              </div>
+              <div className=''>
+                {p.excerpt.rendered}
+              </div>
+              <div className='article-footer'>
+                <Link href={`/p/${p.link}`}><a>Read the post <em>{p.title.rendered}</em></a></Link>
+              </div>
+            </article>
           )
         })}
-      </div>
+      </Layout>
     )
   }
 }
